89264f72fd875a2ecbd9f215a04eedc69acbd454
a-taghva <taghvali99@gmail.com>
2021-04-09 10:20:47 -0400

    start server

--- config/connection.js
+++ config/connection.js
@@ -10,7 +10,9 @@ if (process.env.JAWSDB_URL) {
   // create connection to databas
   sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
-    dialect: 'mysql2',
+    dialect: 'mysql',
     port: 3306
   });
 }
+
+module.exports = sequelize;
